e7edc19e4dc3ef5a0742ee9183961e676649b9e0
Tom Yu <kangleyu@gmail.com>
2017-04-20 22:14:35 +0800

    keep all gulp config in one file

---
+++ gulp.config.js
@@ -0,0 +1,11 @@
+module.exports = function() {
+    var config ={
+        // all js files
+        alljs: [
+            './src/**/*.js',
+            './*.js'
+        ]
+    };
+
+    return config;
+};
