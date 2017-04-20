542f3a10c0ae4df093ccf25a59d87b593da1d1c9
Tom Yu <kangleyu@gmail.com>
2017-04-20 22:47:07 +0800

    use gulp-plumber instead of custom error logger

--- gulpfile.js
+++ gulpfile.js
@@ -20,8 +20,9 @@ gulp.task('styles', ['clean-styles'], function() {
     log('Compiling less --> CSS');
 
     return gulp.src(config.less)
+        .pipe($.plumber())
         .pipe($.less())
-        .on('error', errorLogger)
+        // .on('error', errorLogger)
         .pipe($.autoprefixer({ brosers: ['last 2 version', '> 5%']}))
         .pipe(gulp.dest(config.temp));
 });
@@ -36,13 +37,6 @@ gulp.task('less-watcher', function() {
 });
 
 //////// help functions
-
-function errorLogger(error) {
-    log('*** Start of Error ***');
-    log(error);
-    log('*** End of Error ***');
-    this.emit('end');
-}
 
 function clean(path, done) {
     log('Cleaning: ' + $.util.colors.blue(path));
