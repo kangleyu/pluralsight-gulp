68b7454251b8736176e203c329e33fa7ac771c01
Tom Yu <kangleyu@gmail.com>
2017-04-20 22:05:39 +0800

    add gulp-print to print all source files, and gulp-if for condition task. and yargs for reading parameter from command line

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var util = require('gulp-util');
+var gulpprint = require('gulp-print');
+var gulpif = require('gulp-if');
+var args = require('yargs').argv;
 
 gulp.task('vet', function() {
     log('Analyzing source with JSHint and JSCS');
@@ -9,9 +12,11 @@ gulp.task('vet', function() {
         './src/**/*.js',
         './*.js'
     ])
+    .pipe(gulpif(args.verbose, gulpprint()))
     .pipe(jscs())
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish',{ verbose: true }));
+    .pipe(jshint.reporter('jshint-stylish',{ verbose: true }))
+    .pipe(jshint.reporter('fail'));
 });
 
 ////////
