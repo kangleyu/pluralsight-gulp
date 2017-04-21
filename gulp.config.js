7c68e12642049a3aa1213661c551c57e4520204f
Tom Yu <kangleyu@gmail.com>
2017-04-21 21:52:10 +0800

    auto inject references for html based on wiredep and gulp-inject

--- gulp.config.js
+++ gulp.config.js
@@ -1,5 +1,6 @@
 module.exports = function() {
-    var client ='./src/client/'
+    var client ='./src/client/';
+    var clientApp = client + 'app/';
 
     var config ={
         temp: './.tmp/',
@@ -11,8 +12,34 @@ module.exports = function() {
             './src/**/*.js',
             './*.js'
         ],
+        client: client,
+        index: client + 'index.html',
 
-        less: client + 'styles/styles.less'
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js'
+        ],
+
+        less: client + 'styles/styles.less',
+
+        /**
+         * Bower and NPM locations
+         */
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..'
+        }
+    };
+
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            director: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        }
+        return options;
     };
 
     return config;
