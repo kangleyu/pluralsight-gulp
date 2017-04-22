2986dfe5582b66443dfc2f65af2eebbc1ae9db96
Tom Yu <kangleyu@gmail.com>
2017-04-22 15:58:49 +0800

    add nosync parameter for no syncing choice

--- gulpfile.js
+++ gulpfile.js
@@ -99,7 +99,7 @@ function changeEvent(event) {
 }
 
 function startBrowserSync() {
-    if (browserSync.active) {
+    if (args.nosync || browserSync.active) {
         return;
     }
     log('Starting browser-sync on port' + port);
